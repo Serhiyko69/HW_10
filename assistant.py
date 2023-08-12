1d8fbaa11b0aff1be9a95e5e5edc5f8c9ba7b4ac
Serhiyko69 <magdisyuk1999@gmail.com>
2023-08-12 16:44:06 +0300

    new version

--- assistant.py
+++ assistant.py
@@ -4,53 +4,41 @@ class Field:
     def __init__(self, value=None):
         self.value = value
 
-    def __str__(self):
-        return str(self.value)
-
 class Name(Field):
-    pass
+    def __init__(self, value):
+        super().__init__(value)
 
 class Phone(Field):
-    pass
+    def __init__(self, value):
+        super().__init__(value)
 
 class Record:
-    def __init__(self, name):
-        self.name = Name(name)
-        self.phones = []
+    def __init__(self, name, *phones):
+        self.name = name
+        self.phones = list(phones)
 
     def add_phone(self, phone):
-        self.phones.append(Phone(phone))
+        self.phones.append(phone)
 
     def remove_phone(self, phone):
-        self.phones = [p for p in self.phones if str(p) != phone]
-
-    def edit_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if str(phone) == old_phone:
-                phone.value = new_phone
-
-    def __str__(self):
-        phones_str = ", ".join(map(str, self.phones))
-        return f"Name: {self.name}, Phones: {phones_str}"
+        if phone in self.phones:
+            self.phones.remove(phone)
 
 class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
 
-    def search_records(self, **criteria):
-        results = []
-        for record in self.data.values():
-            match = True
-            for field, value in criteria.items():
-                if field == "phone":
-                    if not any(str(phone) == value for phone in record.phones):
-                        match = False
-                        break
-                elif field == "name":
-                    if str(record.name) != value:
-                        match = False
-                        break
-            if match:
-                results.append(record)
-        return results
+if __name__ == '__main__':
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
 
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+
+    print('All Ok')
